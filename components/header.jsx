8911196018aca824a48d0827c877502f30f74d97
Onwuasoanya James <onwuasoanyajames@gmail.com>
2023-06-05 06:13:50 +0100

    home push

---
+++ components/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+      <h1>J</h1> 
+      
+      <nav>
+          <a href="#"><li>home</li></a>
+          <a href="#"><li>Projects</li></a>
+          <a href="#"><li>About</li></a>
+          <a href="#"><li>Blogs</li></a>
+          <a href="#" id="email">onwuasoanyajames@gmail.com</a>
+      </nav>
+  </header>
+  )
+}
+export default Header
