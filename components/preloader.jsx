04c97daa2f8f07eb2e04d26136145d8d8b48320e
jamiecruise <onwuasoanyajames@gmail.com>
2023-06-15 10:17:22 +0100

    preloader

---
+++ components/preloader.jsx
@@ -0,0 +1,16 @@
+
+const Preloader = ()=>{
+    return(
+        <div className="preloader h-full">
+            <div id="anime-box">
+                Preload my shit
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Preloader
