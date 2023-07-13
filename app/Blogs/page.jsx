069c39511cd83c948e140206695766ea15700c2b
jamiecruise <onwuasoanyajames@gmail.com>
2023-07-13 18:55:27 +0100

    office push07/13(5)-profile

--- app/Blogs/page.jsx
+++ app/Blogs/page.jsx
@@ -15,48 +15,70 @@ const Blog = () => {
     const data =  <div className="relative flex flex-col justify-between lg:h-screen bg-gradient-to-b from-slate-600 to- bg-deepSlate">
         <Nav logo={logo} color={className} prop={ret}/>
         <Mobile_menu /> 
-        <div className="mx-4 mt-5 grid grid-cols-2">
+        <div className="mx-4 mt-6 grid grid-cols-2">
           <div className="p-3 bg-slate-500 rounded-lg mx-1 mb-1">
             <h3 className="px-2">Basics of React Router Dom 6 </h3>
             <div className="px-2">
-              <p>Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
-              <p id='reactions' className=" font-thin text-xs p-2">
-                <span className="p-1">views:{reactions.views}</span>
-                <span className="p-1">comment:{reactions.comments}</span>
-                <span className="p-1">likes:{reactions.likes}</span>
+              <p className="text-sm">Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
+              <p id='reactions' className="flex font-thin text-xs p-2">
+                <span className="p-1">vs:{reactions.views}</span>
+                <span className="p-1">cs:{reactions.comments}</span>
+                <span className="p-1">ls:{reactions.likes}</span>
               </p>
             </div>
           </div>    
           <div className="p-3 bg-slate-500 rounded-lg mx-1 mb-1">
             <h3 className="px-2">Basics of React Router Dom 6 </h3>
             <div className="px-2">
-              <p>Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
-              <p id='reactions' className=" font-thin text-xs p-2">
-                <span className="p-1">views:{reactions.views}</span>
-                <span className="p-1">comments:{reactions.comments}</span>
-                <span className="p-1">likes:{reactions.likes}</span>
+              <p className="text-sm">Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
+              <p id='reactions' className="flex font-thin text-xs p-2">
+                <span className="p-1">vs:{reactions.views}</span>
+                <span className="p-1">cs:{reactions.comments}</span>
+                <span className="p-1">ls:{reactions.likes}</span>
               </p>
             </div>
           </div>    
           <div className="p-3 bg-slate-500 rounded-lg mx-1 mb-1">
             <h3 className="px-2">Basics of React Router Dom 6 </h3>
             <div className="px-2">
-              <p>Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
-              <p id='reactions' className=" font-thin text-xs p-2">
-                <span className="p-1">views:{reactions.views}</span>
-                <span className="p-1">comments:{reactions.comments}</span>
-                <span className="p-1">likes:{reactions.likes}</span>
+              <p className="text-sm">Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
+              <p id='reactions' className="flex font-thin text-xs p-2">
+                <span className="p-1">vs:{reactions.views}</span>
+                <span className="p-1">cs:{reactions.comments}</span>
+                <span className="p-1">ls:{reactions.likes}</span>
               </p>
             </div>
           </div>    
           <div className="p-3 bg-slate-500 rounded-lg mx-1 mb-1">
             <h3 className="px-2">Basics of React Router Dom 6 </h3>
             <div className="px-2">
-              <p>Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
-              <p id='reactions' className=" font-thin text-xs p-2">
-                <span className="p-1">views:{reactions.views}</span>
-                <span className="p-1">comments:{reactions.comments}</span>
-                <span className="p-1">likes:{reactions.likes}</span>
+              <p className="text-sm">Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
+              <p id='reactions' className="flex font-thin text-xs p-2">
+                <span className="p-1">vs:{reactions.views}</span>
+                <span className="p-1">cs:{reactions.comments}</span>
+                <span className="p-1">ls:{reactions.likes}</span>
+              </p>
+            </div>
+          </div>    
+          <div className="p-3 bg-slate-500 rounded-lg mx-1 mb-1">
+            <h3 className="px-2">Basics of React Router Dom 6 </h3>
+            <div className="px-2">
+              <p className="text-sm">Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
+              <p id='reactions' className="flex font-thin text-xs p-2">
+                <span className="p-1">vs:{reactions.views}</span>
+                <span className="p-1">cs:{reactions.comments}</span>
+                <span className="p-1">ls:{reactions.likes}</span>
+              </p>
+            </div>
+          </div>    
+          <div className="p-3 bg-slate-500 rounded-lg mx-1 mb-1">
+            <h3 className="px-2">Basics of React Router Dom 6 </h3>
+            <div className="px-2">
+              <p className="text-sm">Veniam pariatur cillum ullamco proident et id Lorem excepteur ad.</p>
+              <p id='reactions' className="flex font-thin text-xs p-2">
+                <span className="p-1">s:{reactions.views}</span>
+                <span className="p-1">cs:{reactions.comments}</span>
+                <span className="p-1">ls:{reactions.likes}</span>
               </p>
             </div>
           </div>    
