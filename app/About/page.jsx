eb27c50d9f72c9bb82121d859a8f4f5799b273e4
jamiecruise <onwuasoanyajames@gmail.com>
2023-07-21 07:58:06 +0100

    office push07/21(1)-profile

--- app/About/page.jsx
+++ app/About/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 import Nav from '@components/header'
-// import Mobile_menu from '@components/mobile_menu'
+import Mobile_menu from '@components/mobile_menu'
 import ret from '@components/function'
 import Preloader from '@components/preloader'
 import { comment } from 'postcss'
@@ -11,7 +11,7 @@ const About = () => {
     const className = "text-blue-300 font-[Tahoma] text-3xl m-0 pl-4"
     const data =  <div id="container" className="relative flex flex-col justify-between h-vh92 lg:h-screen bg-gradient-to-b from-gray-600 to- bg-deepSlate">
           <Nav logo={logo} color={className} prop={ret}/>
-          {/* <Mobile_menu />    */}
+          <Mobile_menu />   
           
       </div>
   return (
