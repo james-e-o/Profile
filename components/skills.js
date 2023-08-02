b2f8b8ba7e048e5c8f4011ab538dc793ca4b8983
jamiecruise <onwuasoanyajames@gmail.com>
2023-08-02 18:55:37 +0100

    office push08/2-profile

---
+++ components/skills.js
@@ -0,0 +1,53 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div className="container">
+        <div className="core1">
+            <div className="hexagon-wrap w1">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+            <div className="hexagon-wrap w2">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+            <div className="hexagon-wrap w3">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+            <div className="hexagon-wrap w4">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+            <div className="hexagon-wrap w5">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+            <div className="hexagon-wrap w6">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+            <div className="hexagon-wrap w7">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+            <div className="hexagon-wrap w8">
+                <div className="hextri tr1"></div>
+                <div className="hexsq">welcome</div>
+                <div className="hextri tr2"></div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Skills
